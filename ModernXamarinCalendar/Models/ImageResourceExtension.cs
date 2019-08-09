c193b01b3f008729d42529d85dfd5f79caff30be
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-08-09 20:40:53 +0800

    Ensure that images required are in class library

---
+++ ModernXamarinCalendar/Models/ImageResourceExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ModernXamarinCalendar.Models
+{
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        //* Public Properties
+        public string Source { get; set; }
+
+        //* Interface Implementation
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(Source, 
+                typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+}
